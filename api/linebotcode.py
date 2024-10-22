15f8094a690f6b5278cca33f6ab72e4595d761c9
bo306 <123080633+bo306@users.noreply.github.com>
2024-10-22 15:00:04 +0800

    Update linebotcode.py

--- api/linebotcode.py
+++ api/linebotcode.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler,
+from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import (MessageEvent, TextMessage, TextSendMessage,CarouselColumn,CarouselTemplate, ButtonsTemplate, ConfirmTemplate,MessageAction, URIAction, ImageCarouselColumn, ImageCarouselTemplate)
-
+from linebot.models import (MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, CarouselColumn,
+                            CarouselTemplate, MessageAction, URIAction, ImageCarouselColumn, ImageCarouselTemplate,
+                            ImageSendMessage, ButtonsTemplate, ConfirmTemplate)
 import os
-import html  # 使用標準庫的 escape 函數
+import requests
+from bs4 import BeautifulSoup
+import random
 
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
@@ -17,10 +20,12 @@ def home():
 
 @app.route("/webhook", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
+    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
+    # handle webhook body
     try:
         line_handler.handle(body, signature)
     except InvalidSignatureError:
@@ -29,100 +34,101 @@ def callback():
 
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+
     if event.message.text == 'confirm':
         confirm_template = TemplateSendMessage(
-            alt_text='confirm template',
-            template=ConfirmTemplate(
-                text='drink coffee?',
-                actions=[
+            alt_text = 'confirm template',
+            template = ConfirmTemplate(
+                text = 'drink coffee?',
+                actions = [
                     MessageAction(
-                        label='yes',
-                        text='yes'),
+                        label = 'yes',
+                        text = 'yes'),
                     MessageAction(
-                        label='no',
-                        text='no')]
+                        label = 'no',
+                        text = 'no')]
+                )
             )
-        )
         line_bot_api.reply_message(event.reply_token, confirm_template)
 
 
-    # 按鈕樣板
+    #按鈕樣板
     if event.message.text == 'button':
         buttons_template = TemplateSendMessage(
-            alt_text='buttons template',
-            template=ButtonsTemplate(
+            alt_text = 'buttons template',
+            template = ButtonsTemplate(
                 thumbnail_image_url='https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg',
-                title='Brown Cafe',
-                text='Enjoy your coffee',
-                actions=[
+                title = 'Brown Cafe',
+                text = 'Enjoy your coffee',
+                actions = [
                     MessageAction(
-                        label='咖啡有什麼好處',
-                        text='讓人有精神'),
+                        label = '咖啡有什麼好處',
+                        text = '讓人有精神'),
                     URIAction(
-                        label='伯朗咖啡',
-                        uri='https://www.mrbrown.com.tw/')]
+                        label = '伯朗咖啡',
+                        uri = 'https://www.mrbrown.com.tw/')]
+                )
             )
-        )
 
         line_bot_api.reply_message(event.reply_token, buttons_template)
 
 
-    # carousel 樣板
+    #carousel樣板
     if event.message.text == 'carousel':
         carousel_template = TemplateSendMessage(
-            alt_text='carousel template',
-            template=CarouselTemplate(
-                columns=[
-                    # 第一個
+            alt_text = 'carousel template',
+            template = CarouselTemplate(
+                columns = [
+                    #第一個
                     CarouselColumn(
-                        thumbnail_image_url='https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg',
-                        title='this is menu1',
-                        text='menu1',
-                        actions=[
+                        thumbnail_image_url = 'https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg',
+                        title = 'this is menu1',
+                        text = 'menu1',
+                        actions = [
                             MessageAction(
-                                label='咖啡有什麼好處',
-                                text='讓人有精神'),
+                                label = '咖啡有什麼好處',
+                                text = '讓人有精神'),
                             URIAction(
-                                label='伯朗咖啡',
-                                uri='https://www.mrbrown.com.tw/')]),
-                    # 第二個
+                                label = '伯朗咖啡',
+                                uri = 'https://www.mrbrown.com.tw/')]),
+                    #第二個
                     CarouselColumn(
-                        thumbnail_image_url='https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg',
-                        title='this is menu2',
-                        text='menu2',
-                        actions=[
+                        thumbnail_image_url = 'https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg',
+                        title = 'this is menu2',
+                        text = 'menu2',
+                        actions = [
                             MessageAction(
-                                label='咖啡有什麼好處',
-                                text='讓人有精神'),
+                                label = '咖啡有什麼好處',
+                                text = '讓人有精神'),
                             URIAction(
-                                label='伯朗咖啡',
-                                uri='https://www.mrbrown.com.tw/')])
+                                label = '伯朗咖啡',
+                                uri = 'https://www.mrbrown.com.tw/')])
                 ])
-        )
+            )
 
         line_bot_api.reply_message(event.reply_token, carousel_template)
 
 
-    # image carousel 樣板
+    #image carousel樣板
     if event.message.text == 'image carousel':
         image_carousel_template = TemplateSendMessage(
-            alt_text='image carousel template',
-            template=ImageCarouselTemplate(
-                columns=[
-                    # 第一張圖
+            alt_text = 'image carousel template',
+            template = ImageCarouselTemplate(
+                columns = [
+                    #第一張圖
                     ImageCarouselColumn(
-                        image_url='https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg',
-                        action=URIAction(
-                            label='伯朗咖啡',
-                            uri='https://www.mrbrown.com.tw/')),
-                    # 第二張圖
+                        image_url = 'https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg',
+                        action = URIAction(
+                            label = '伯朗咖啡',
+                            uri = 'https://www.mrbrown.com.tw/')),
+                    #第二張圖
                     ImageCarouselColumn(
-                        image_url='https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg',
-                        action=URIAction(
-                            label='伯朗咖啡',
-                            uri='https://www.mrbrown.com.tw/'))
+                        image_url = 'https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg',
+                        action = URIAction(
+                            label = '伯朗咖啡',
+                            uri = 'https://www.mrbrown.com.tw/'))                       
                 ])
-        )
+            )
 
         line_bot_api.reply_message(event.reply_token, image_carousel_template)
 
